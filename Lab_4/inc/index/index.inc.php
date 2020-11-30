68b25d66b09ddab7fce9fa46e738eba6b68dd592
WildVileWing <wildvilewingg@gmail.com>
2020-11-30 11:58:18 +0300

    Lab_4

---
+++ Lab_4/inc/index/index.inc.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3			+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="CSS/main.css"> 
+	</head>
+	
+		<body>	
+				<br>
+	    		<div class="row">
+	    			<div class="col-2"></div>
+	    			<h1>Интересные новости и события.</h1>
+	    		</div>
+	    		<br>
+	    		<div class="row">
+	    			<div class="col-2"></div>
+	    			
+	    			<div class="card" style=" width: 18rem; border: 1px black solid;">
+						<img src="img/Img1.jpg" class="card-img-top" alt="">
+						<div class="card-body">
+							<h5 class="card-title">Новые тренажеры</h5>
+							<p class="card-text">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text.</p>
+							
+							
+						</div>
+					</div>
+	    			<div class="col-1"></div>
+	    			<div class="card" style="width: 18rem; border: 1px black solid;">
+						<img src="img/Img2.jpg" class="card-img-top" alt="">
+						<div class="card-body">
+							<h5 class="card-title">Новый тренажер</h5>
+							<p class="card-text">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text.</p>
+							
+							
+						</div>
+					</div>
+					<div class="col-1"></div>
+	    			<div class="card" style=" width: 18rem; border: 1px black solid;">
+						<img src="img/First.png" class="card-img-top" alt="">
+						<div class="card-body">
+							<h5 class="card-title">Уникальная гантеля в 300кг</h5>
+							<p class="card-text">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text.</p>
+							
+						</div>
+					</div>
+	    		</div>
+	    		<div class="row">
+	    			<div class="col-2"></div>
+	    			<div class="card" style=" width: 18rem; border: 1px black solid;">
+						<img src="img/God.png" class="card-img-top" alt="">
+						<div class="card-body">
+							<h5 class="card-title">Наш новый тренер научит вас чему-то новому</h5>
+							<p class="card-text">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text.</p>
+							
+						</div>
+					</div>
+	    			<div class="col-1"></div>
+	    			<div class="card" style=" width: 18rem; border: 1px black solid;">
+						<img src="img/Img3.jpg" class="card-img-top" alt="">
+						<div class="card-body">
+							<h5 class="card-title">Полотенца со вкусом лаванды</h5>
+							<p class="card-text">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text.</p>
+							
+						</div>
+					</div>
+	    		</div>
+		</body>
+</html>	
---
+++ Lab_4/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		
+		<title>Атлетический клуб</title>
+	</head>
+	<!-- Header Begin -->
+	<body>
+		<div class="container-fluid">
+				<nav class="navbar navbar-expand-md sticky-top navbar-light bg-warning font-weight-bold">
+					<div class="collapse navbar-collapse">
+						<ul class="navbar-nav mr-auto">
+							<li class = "nav-item active">
+								<a class = "nav-link" href = "/Lab_4/index.php">Новости<span class = "sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href = "/Lab_4/index.php?id=about">О нас<span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item active">	
+								<a class="nav-link" href="/Lab_4/index.php?id=gbook"> Гостевая книга <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href="/Lab_4/index.php?id=contact">Абонименты<span class="sr-only">(current)</span></a>
+							</li>
+						</ul>		
+					</div>
+			</nav>
+			
+			<img src="img/Second.jpg" style = "width: 100%; height: 300px;" class="img-fluid">
+			
+			<div class="row bg-warning" style="height: 80px; padding-top: 10px; margin: 0px;">		   
+			    <div class="col-4" style="padding-top: 5px;text-align: right;"><h2> Абонимент </h2></div>
+			    <div class="col-1" style="padding-top: 10px">
+			      	<input type="text" class="form-control" placeholder="Начало">	 
+		    	</div>
+		    	<div class="col-1" style="padding-top: 10px">
+			      	<input type="text" class="form-control " placeholder="Конец">	 
+		    	</div>
+		    	<div class="col-1" style="padding-top: 10px">
+			      	<input type="text" class="form-control"  placeholder="Цена">	 
+		    	</div>
+		    	<button  type="button" class="btn btn-success"  style="height: 40px; margin-top: 10px" ><h4> Поиск </h4></button>
+	    	</div>
+	    	<!-- Header End -->
+	    	
+	    	<!-- Content Begin -->
+	    	
+ 			<?php 
+	    		include 'routing.php';
+	    	?>
+	    	
+	    	<!-- Content End -->
+	    	
+	    	 <!-- Footer -->
+	    	<div class="container-fluid bg-warning" style="margin-top: 40px;">
+	    		<div class="row" style = "text-align: center;">
+					<div class = "col-2"></div>
+	    			<div class ="col-3 h4 text-dark" style = "padding-top: 10px;">Сайт работает с 0:00 до 23:59</div>
+	    			<div class ="col-2"> <a href = "#" class="h4 text-dark nav-link"> Разработчики</a></div>
+	    			<div class ="col-2"> <a href = "#" class = "h4 text-dark nav-link"> Тех-поддержка</a></div>
+	    			<div class ="col-2 h4 text-dark nav-link" style = "padding-top: 10px;">+7 979 797 97 97</div>
+	    		</div>
+	    	</div>
+		</div>
+	</body>
+</html>
